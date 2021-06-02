2aaea879f1180f5fa675158745a929ff2098344a
JohnCgit <jean.cornillon@cpe.fr>
2021-06-02 14:27:42 +0200

    first connection

---
+++ js/apiApp.js
@@ -0,0 +1,22 @@
+var port = 8081;
+
+function loadRessource(source, method, body) {
+    var xhttp = new XMLHttpRequest();
+    source = `http://127.0.0.1:${port}`+source;
+    xhttp.open(method, source, false);
+    xhttp.overrideMimeType("application/json");
+    if (body != null) {
+        xhttp.setRequestHeader("Content-type", "application/json")
+        xhttp.send(body);
+    }
+    else {
+        xhttp.send();
+    }
+    if (xhttp.status == 200) {
+        return xhttp.response;
+    }
+};
+
+function getListFire(){
+    return JSON.parse(loadRessource('http://127.0.0.1:8081/fire',"GET"));
+};
