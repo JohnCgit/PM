260295cff67a5bee9b44d5605eb65af40d82edf6
Jagen22 <78451338+Jagen22@users.noreply.github.com>
2021-06-02 14:21:08 +0200

    Upload Proj_Emergency - Back-end

---
+++ Proj_Emergency/src/main/java/com/webAppEmergency/AppFire.java
@@ -0,0 +1,12 @@
+package com.webAppEmergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFire {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppFire.class,args);
+	}
+}
