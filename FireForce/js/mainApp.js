921924e1b6a86c3fec0ae1a622b86979cdab23cd
JohnCgit <78431261+JohnCgit@users.noreply.github.com>
2021-06-02 14:33:11 +0200

    Delete FireForce directory

--- FireForce/js/mainApp.js
+++
@@ -1,2 +0,0 @@
-const FireApp = new FireApp(L.featureGroup(),Array(),getFireList());
-const displayApp = new displayApp(L.map(document.getElementById("mapid")).setView([45.752433, 4.834328], 10));
--- FireForce/js/map.js
+++
@@ -1,5 +0,0 @@
-class map{
-    constructor(){
-        L.map(document.getElementById("mapid")).setView([45.752433, 4.834328], 10);
-    }
-}
