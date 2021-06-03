ca4f2b0be1e84f6a11ca56af688624d46618359d
tp <tp@Vm-Light>
2021-06-03 11:55:17 +0200

    Fin de matinée

--- Java/Assignation/src/main/java/com/webAppEmergency/AppAssignation.java
+++ Java/Assignation/src/main/java/com/webAppEmergency/AppAssignation.java
@@ -10,16 +10,16 @@ public class AppAssignation {
 	public static void main(String [] args) {
 		SpringApplication.run(AppAssignation.class, args);
 
-		MainRunnable mainRunnable;
-		Thread displayThread;
-		
-		//Create a Runnable is charge of executing cyclic actions 
-		mainRunnable=new MainRunnable();
-		
-		// A Runnable is held by a Thread which manage lifecycle of the Runnable
-		displayThread=new Thread(mainRunnable);
-		
-		// The Thread is started and the method run() of the associated DisplayRunnable is launch
-		displayThread.start();
+//		MainRunnable mainRunnable;
+//		Thread displayThread;
+//		
+//		//Create a Runnable is charge of executing cyclic actions 
+//		mainRunnable=new MainRunnable();
+//		
+//		// A Runnable is held by a Thread which manage lifecycle of the Runnable
+//		displayThread=new Thread(mainRunnable);
+//		
+//		// The Thread is started and the method run() of the associated DisplayRunnable is launch
+//		displayThread.start();
 	}
 }
