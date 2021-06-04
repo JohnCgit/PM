4e439a2c63924ccd05ef3ae017bfa06b545d9993
tp <tp@Vm-Light>
2021-06-04 08:37:14 +0200

    update boot appCaserne

--- Java/Caserne/src/main/java/com/webAppEmergency/AppCaserne.java
+++ Java/Caserne/src/main/java/com/webAppEmergency/AppCaserne.java
@@ -1,11 +1,19 @@
 package com.webAppEmergency;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.webAppEmergency.Caserne.CaserneService;
+
 @SpringBootApplication
 public class AppCaserne {
+	
+	@Autowired
+	static CaserneService cService;
+	
 	public static void main(String [] args) {
 		SpringApplication.run(AppCaserne.class, args);
+		cService.getCaserneLyon();
 	}
 }
