3300e57dbbc70a768c13a82fa2cbce7b1197bbe0
tp <tp@Vm-Light>
2021-06-04 08:57:03 +0200

    Boot Camion

--- Java/Caserne/src/main/java/com/webAppEmergency/AppCaserne.java
+++ Java/Caserne/src/main/java/com/webAppEmergency/AppCaserne.java
@@ -1,9 +1,14 @@
 package com.webAppEmergency;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.webAppEmergency.Caserne.Caserne;
 import com.webAppEmergency.Caserne.CaserneService;
 
 @SpringBootApplication
@@ -14,6 +19,10 @@ public class AppCaserne {
 	
 	public static void main(String [] args) {
 		SpringApplication.run(AppCaserne.class, args);
-		cService.getCaserneLyon();
+		List<Caserne> ListC = new ArrayList<>();
+		ListC.add(new Caserne(1, 50, 50, "oiui", Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+		cService.initCaserne(ListC);
 	}
 }
+
+
