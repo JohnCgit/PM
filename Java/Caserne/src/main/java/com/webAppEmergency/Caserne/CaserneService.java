3242544c7b0645378617182f590db9435b3dfb7a
tp <tp@Vm-Light>
2021-06-04 11:11:04 +0200

    Protection de initCaserne

--- Java/Caserne/src/main/java/com/webAppEmergency/Caserne/CaserneService.java
+++ Java/Caserne/src/main/java/com/webAppEmergency/Caserne/CaserneService.java
@@ -54,11 +54,14 @@ public class CaserneService {
 	}
 	
 	public void initCaserne() {
-		List<Caserne> ListC = new ArrayList<>();
-		ListC.add(new Caserne(4.855997011435769, 45.8295890450427, "Centre d'Intervention des 2 Fontaines", Arrays.asList(), Arrays.asList(1, 2, 3), 15));
-		ListC.add(new Caserne(4.819356307585479, 45.89601102593775, "Centre d'Intervention de Genay-Neuville", Arrays.asList(), Arrays.asList(4, 5), 10));
-		ListC.add(new Caserne(4.715254999410806, 45.824827365743985, "Centre d'Intervention de La Tour-de-Salvagny", Arrays.asList(), Arrays.asList(6, 7, 8, 9), 12));
-		for (Caserne c: ListC) {cRepo.save(c);}
+		if(cRepo.findAll().isEmpty()) {
+			List<Caserne> ListC = new ArrayList<>();
+			ListC.add(new Caserne(4.855997011435769, 45.8295890450427, "Centre d'Intervention des 2 Fontaines", Arrays.asList(), Arrays.asList(1, 2, 3), 15));
+			ListC.add(new Caserne(4.819356307585479, 45.89601102593775, "Centre d'Intervention de Genay-Neuville", Arrays.asList(), Arrays.asList(4, 5), 10));
+			ListC.add(new Caserne(4.715254999410806, 45.824827365743985, "Centre d'Intervention de La Tour-de-Salvagny", Arrays.asList(), Arrays.asList(6, 7, 8, 9), 12));
+			for (Caserne c: ListC) {cRepo.save(c);}
+		}
+
 
 	}
 	
